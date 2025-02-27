08fff753d76ef53cd73bec4d378d0e26343a4ada
gksdyd <gksdyd@naver.com>
2025-02-28 01:59:02 +0900

    1. 필터 수정 완료

--- Workspace/optimal-html/assets/js/main.js
+++ Workspace/optimal-html/assets/js/main.js
@@ -937,15 +937,15 @@
     function price_slider() {
         $("#slider-range").slider({
             range: true,
-            min: 12,
-            max: 200,
-            values: [0, 100],
+            min: 0,
+            max: 400000,
+            values: [0, 400000],
             slide: function (event, ui) {
-                $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+                $("#amount").val("￦" + ui.values[0] + " - ￦" + ui.values[1]);
             }
         });
-        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-                " - $" + $("#slider-range").slider("values", 1));
+        $("#amount").val("￦" + $("#slider-range").slider("values", 0) +
+                " - ￦" + $("#slider-range").slider("values", 1));
     }
     price_slider();
 
