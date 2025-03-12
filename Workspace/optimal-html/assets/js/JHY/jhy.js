cb0b75089824fcb87decf7c3aaa659321d2dafaa
gksdyd <gksdyd009@naver.com>
2025-03-12 21:45:39 +0900

    1. 상품 페이지 정리

--- Workspace/optimal-html/assets/js/JHY/jhy.js
+++ Workspace/optimal-html/assets/js/JHY/jhy.js
@@ -119,3 +119,82 @@ window.addEventListener('DOMContentLoaded', function() {
         location.href='http://127.0.0.1:5500/Workspace/optimal-html/checkout-success.html';
     }
   }
+
+  $(function () {
+    var $pswp = $('.pswp')[0],
+            image = [],
+            getItems = function () {
+                var items = [];
+                $('.lightboximages a').each(function () {
+                    var $href = $(this).attr('href'),
+                            $size = $(this).data('size').split('x'),
+                            item = {
+                                src: $href,
+                                w: $size[0],
+                                h: $size[1]
+                            };
+                    items.push(item);
+                });
+                return items;
+            };
+    var items = getItems();
+
+    $.each(items, function (index, value) {
+        image[index] = new Image();
+        image[index].src = value['src'];
+    });
+    $('.prlightbox').on('click', function (event) {
+        event.preventDefault();
+
+        var $index = $(".active-thumb").parent().attr('data-slick-index');
+        $index++;
+        $index = $index - 1;
+
+        var options = {
+            index: $index,
+            bgOpacity: 0.7,
+            showHideOpacity: true
+        };
+        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        lightBox.init();
+    });
+});
+$(function () {
+    var $pswp = $('.pswp')[0],
+            image = [],
+            getItems = function () {
+                var items = [];
+                $('.reviewimages a').each(function () {
+                    var $href = $(this).attr('href'),
+                            $size = $(this).data('size').split('x'),
+                            item = {
+                                src: $href,
+                                w: $size[0],
+                                h: $size[1]
+                            };
+                    items.push(item);
+                });
+                return items;
+            };
+    var items = getItems();
+
+    $.each(items, function (index, value) {
+        image[index] = new Image();
+        image[index].src = value['src'];
+    });
+    $('.reviewbox').on('click', function (event) {
+        event.preventDefault();
+
+        var $index = $(".active-thumb").parent().attr('data-slick-index');
+        $index++;
+        $index = $index - 1;
+
+        var options = {
+            index: $index,
+            bgOpacity: 0.7,
+            showHideOpacity: true
+        };
+        var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+        lightBox.init();
+    });
+});
